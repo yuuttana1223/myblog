ef7f143181364b2fff2a01eb70b58bd858dc0f96
vagrant <vagrant@localhost.localdomain>
2020-12-28 01:16:03 +0900

    finish blog

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  # @post.commentsとするため
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 3, message: 'Too short to post!' }
+  validates :body, presence: true
+end
